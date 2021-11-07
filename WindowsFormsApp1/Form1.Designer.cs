685ad4195adf3895f1fe16e15694d3e5726479d9
こまめ <54506591+Komame3@users.noreply.github.com>
2021-11-07 23:37:39 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(12, 22);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 19);
+            this.txtValue1.TabIndex = 0;
+            this.txtValue1.TextChanged += new System.EventHandler(this.txtValue1_TextChanged);
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(138, 22);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 19);
+            this.txtValue2.TabIndex = 1;
+            this.txtValue2.TextChanged += new System.EventHandler(this.txtValue2_TextChanged);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(352, 22);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 19);
+            this.txtAnswer.TabIndex = 2;
+            this.txtAnswer.TextChanged += new System.EventHandler(this.txtAnswer_TextChanged);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(253, 20);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "計算する";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.listBox1.Location = new System.Drawing.Point(118, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(14, 52);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(253, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "自動";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 96);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //計算するボタンをクリックされた時に来る
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calc(true);
+        }
+        private void calc(bool autoFlg) 
+        { 
+            if (autoFlg == false)
+            {
+                //チェックされていない　＝　手動
+                return;
+            }
+            int value1 = 0;
+            int value2 = 0;
+
+            if (String.IsNullOrEmpty(txtValue1.Text))
+            {
+                MessageBox.Show("入力欄１が空です");
+                return;
+            }
+            /* Javaにありがちな記述
+            String a;
+            //if (a.Equals(""))
+            if ("".Equals(a))
+            {
+
+            }
+            */
+
+            /*
+            if (txtValue1.Text.Equals(""))
+            {
+                MessageBox.Show("入力欄１が空です");
+                return;
+            }
+            */
+
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("演算子を選択してください");
+                return;
+            }
+
+            if (txtValue2.Text.Equals(""))
+            {
+                MessageBox.Show("入力欄２が空です");
+                return;
+            }
+
+            try
+            {
+                value1 = int.Parse(txtValue1.Text);
+                value2 = int.Parse(txtValue2.Text);
+            } catch (Exception ex)
+            {
+                //エラー処理
+                MessageBox.Show("エラーだよ");
+                return;
+            }
+            //文字列→数値変換 txtValue1はString型
+            //int value1 = int.Parse(txtValue1.Text);
+            //int value2 = int.Parse(txtValue2.Text);
+
+            //int answer = value1 + value2;
+
+            //listBox1.SelectedIndex;
+            //MessageBox.Show(listBox1.SelectedIndex.ToString());
+            //MessageBox.Show(listBox1.SelectedItem.ToString());
+
+            int answer = 0;
+            if (listBox1.SelectedIndex == 0)
+            { //+の場合
+                answer = value1 + value2;
+            }
+            if (listBox1.SelectedIndex == 1)
+            {
+                answer = value1 - value2;
+            }
+            if (listBox1.SelectedIndex == 2)
+            {
+                answer = value1 * value2;
+            }
+            if (listBox1.SelectedIndex == 3)
+            {
+                answer = value1 / value2;
+            }
+
+            /* SelectedItemの場合
+            int answer = 0; //if文の内部だとカッコから出た瞬間に使えなくなってしまう
+    
+            if (listBox1.SelectedItem.Equals("+")) //Equals→文字列が一致してるか比較
+            {
+                answer = value1 + value2;
+            }
+            if (listBox1.SelectedItem.Equals("-"))
+            {
+                answer = value1 - value2;
+            }
+            if (listBox1.SelectedItem.Equals("*"))
+            {
+                answer = value1 * value2;
+            }
+            if (listBox1.SelectedItem.Equals("/"))
+            {
+                answer = value1 / value2;
+            }
+            */
+
+            //txtAnswer.TextはString型　→　answerをintからstringに変換
+            txtAnswer.Text = answer.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAnswer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtValue1_TextChanged(object sender, EventArgs e)
+        {
+            calc(checkBox1.Checked);
+        }
+
+        private void txtValue2_TextChanged(object sender, EventArgs e)
+        {
+            calc(checkBox1.Checked);
+        }
+    }
+}
